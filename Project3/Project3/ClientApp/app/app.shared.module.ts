3dfc2043bffd8bc376fba101ed4f002eb2186bb9
nguyen duy <nguyenduysoict.2310@gmail.com>
2019-04-11 00:24:33 +0700

    home code

--- Project3/Project3/ClientApp/app/app.shared.module.ts
+++ Project3/Project3/ClientApp/app/app.shared.module.ts
@@ -3,26 +3,37 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
+import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
 
 import { AppComponent } from './components/app/app.component';
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+registerLocaleData(en);
 @NgModule({
     declarations: [
         AppComponent,
         NavMenuComponent,
-        HomeComponent
+        HomeComponent,
+        LoginComponent
     ],
     imports: [
         CommonModule,
         HttpModule,
         FormsModule,
+        NgZorroAntdModule,
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
+            { path: 'login', component: LoginComponent },
             { path: '**', redirectTo: 'home' }
         ])
+    ],
+    providers: [
+        { provide: NZ_I18N, useValue: en_US }
     ]
 })
 export class AppModuleShared {
